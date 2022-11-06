9f31dc2c6f2c3fcac26784af0c11a57f4e873536
Vivek Krishna <103879000+vivek0153@users.noreply.github.com>
2022-11-06 19:05:43 +0530

    Update Q-4.22.c

--- Q-4.22.c
+++ Q-4.22.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<pthread.h>
 int arr[50],n,i;
-
+int min,max;
+float avg;
 void *th()
 {
 	int sum=0;
 	float average;
-	printf("enter your number :=");
+	printf("Enter number of numbers:\n");
 	scanf("%d",&n);
+	printf("Enter numbers:\n");
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&arr[i]);
@@ -17,7 +19,7 @@ void *th()
 			sum=sum+arr[i];
 		}
 	average=sum/n;
-	printf("The average value is:%f",average);
+	avg=average;
 }
 void *th1()
 {
@@ -31,8 +33,7 @@ void *th1()
 			temp=arr[i];
 			}
 		}
-	printf("\nThe Minimum  value is:=%d",temp);
-
+		min=temp;
 }
 void *th2()
 {
@@ -45,22 +46,25 @@ void *th2()
 			temp=arr[i];
 			}
 		}
-	printf("\nThe Maximum  value is:=%d",temp);
+		max=temp;
+	
 	}
 int main()
 {
-int n,i;
 pthread_t t1;
 pthread_t t2;
 pthread_t t3;
-	n=pthread_create(&t1,NULL,&th,NULL);
+	pthread_create(&t1,NULL,&th,NULL);
 	pthread_join(t1,NULL);
-	//printf("\n done and my value is %d",n);
-	n=pthread_create(&t2,NULL,&th1,NULL);
+	printf("This Thread is done\n");
+	printf("The average value is:%f\n",avg);
+	pthread_create(&t2,NULL,&th1,NULL);
         pthread_join(t2,NULL);
-	//printf("\n done and my value is %d",n);
-	n=pthread_create(&t3,NULL,&th2,NULL);
+	printf("This Thread is done\n");
+	printf("The Minimum  value is:%d\n",min);
+	pthread_create(&t3,NULL,&th2,NULL);
         pthread_join(t3,NULL);
-	//printf("\n done and my value is %d",n);
+	printf("This Thread is done\n");
+	printf("The Maximum  value is:%d\n",max);
 
 }
